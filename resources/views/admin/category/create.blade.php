3977a27d7fbc2b8aedd6a264088f0b88af1c234e
mason.hows11 <mason.hows11@gmail.com>
2023-12-08 08:13:47 +0330

    create category view updated

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -32,7 +32,7 @@
 
                 <div class="col-sm-6">
                     <div class="mt-3">
-                        <label for=title" class="form-label">{{ __('messages.delivery_title') }}</label>
+                        <label for=title" class="form-label">{{ __('messages.title') }}</label>
                         <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
                     </div>
                     @error('title')
@@ -44,47 +44,53 @@
 
 
                 <div class="col-sm-6">
-                    <div class="mt-3">
-                        <label for="delivery_amount" class="form-label">{{ __('messages.delivery_amount') }}</label>
-                        <input type="text" class="form-control" dir="ltr" id="delivery_amount" name="delivery_amount" value="{{ old('delivery_amount') }}">
+                    <div class="mb-3 mt-3">
+                        <label for="status" class="form-label">وضعیت دسته بندی:</label>
+                        <select class="form-control" wire:model.lazy="status" id="status">
+                            <option>انتخاب کنید</option>
+                            <option value="0">{{ __('messages.deactivate') }}</option>
+                            <option value="1">{{ __('messages.active') }}</option>
+                        </select>
+
+                        @error('status')
+                        <div class="alert alert-danger mt-3">{{ $message}}</div>
+                        @enderror
                     </div>
-                    @error('delivery_amount')
-                    <div class="alert alert-danger mt-3">
-                        {{ $message }}
-                    </div>
-                    @enderror
+
                 </div>
 
 
                 <div class="col-sm-6">
-                    <div class="mt-3">
-                        <label for="delivery_time" class="form-label">{{ __('messages.delivery_time') }}</label>
-                        <input type="text" class="form-control" dir="ltr" id="delivery_time" name="delivery_time" value="{{ old('delivery_time') }}">
-                    </div>
-                    @error('delivery_time')
-                    <div class="alert alert-danger mt-3">
-                        {{ $message }}
-                    </div>
-                    @enderror
-                </div>
-
-
-                <div class="col-sm-6">
-                    <div class="mt-3">
-                        <label for="delivery_time_unit" class="form-label">{{ __('messages.delivery_time_unit') }}</label>
-                        <select name="delivery_time_unit" class="form-select" id="delivery_time_unit">
-                            <option></option>
-                            <option value="ساعت">ساعت</option>
-                            <option value="روز">روز</option>
-                            <option value="ماه">ماه</option>
-                            <option value="سال">سال</option>
+                    <div class=" mb-3 mt-3">
+                        <label for="parent" class="form-label">انتخاب دسته بندی والد:</label>
+                        <select class="form-control" wire:model.lazy="parent" id="parent">
+                            <option value="null">فاقد دسته بندی</option>
+                            @foreach($categories as $item)
+                                <option value="{{ $item->id }}">{{ $item->title_persian }}</option>
+                            @endforeach
                         </select>
                     </div>
-                    @error('delivery_time_unit')
-                    <div class="alert alert-danger mt-3">
-                        {{ $message }}
+                </div>
+
+
+                <div class="col-lg-4 col-md-4 col-sm-4">
+                    <div class="row d-flex flex-column justify-content-center align-content-center">
+                        <div class="col-lg-8 top-banner-section">
+                            <img src="{{  asset('dash/images/no-image-icon-23494.png') }}"
+                                 id="image_view"
+                                 class="img-thumbnail" height="300" width="300" alt="image">
+                        </div>
+                        <div class="col-lg-5">
+                            <label for="image_select" class="mt-5 form-label">{{ __('messages.image') }}</label>
+                            <input type="file" class="form-control" accept="image/png, image/jpeg , image/jpg ,image/gif" id="image_select" name="image_path">
+                            @error('image_path')
+                            <div class="alert alert-danger mt-3">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
                     </div>
-                    @enderror
+
                 </div>
 
                 <div class="col-12 discount-common-save">
@@ -98,6 +104,24 @@
         </form>
     </div>
 @endsection
+@push('dash_custom_script')
+    <script>
+        $(document).ready(function () {
+            const input = document.getElementById("image_select");
+            const previewImage = document.getElementById("image_view");
+            input.addEventListener("change", function () {
+                const file = this.files[0];
+                if (file) {
+                    const reader = new FileReader();
+                    reader.addEventListener("load", function () {
+                        previewImage.setAttribute("src", this.result);
+                    });
+                    reader.readAsDataURL(file);
+                }
+            });
+        })
+    </script>
+@endpush
 
 
 
