23bec7f597147de7a4d3ed8f71ae6668c767188a
mason.hows11 <mason.hows11@gmail.com>
2023-12-08 21:02:01 +0330

    add image service

--- app/Http/Controllers/Admin/Category/AdminCategoryController.php
+++ app/Http/Controllers/Admin/Category/AdminCategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Services\image\ImageServiceSave;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
@@ -31,16 +32,13 @@ class AdminCategoryController extends Controller
             $category = new Category();
 
             if ($request->hasFile('image_path')) {
-                $image_extension = $request->image_path->getClientOriginalExtension();
-                // create image name
-                $path = 'UIMG' . '_' . date('YmdHis') . '_' . uniqid('img', true) . '.' . $image_extension;
-                // save image with given name
-                $request->image_path->storeAs('images/category/', $path, 'public');
-                $category->image_path = $path;
+                $imageSave = new ImageServiceSave();
+                $image_path =  $imageSave->customSavePublicPath($request->image_path,'category');
+                $category->image_path = $image_path;
+
             }
 
-            if ($request->has('parent'))
-            {
+            if ($request->has('parent')) {
                 $category->title = $request->title;
                 $category->status = $request->status;
                 $category->slug = $request->slug;
@@ -71,6 +69,44 @@ class AdminCategoryController extends Controller
 
     public function update(Request $request)
     {
+
+        $request->validate([
+            'title' => ['required','unique:categories','min:2', 'max:30'],
+            'slug' => ['required','min:2', 'max:30'],
+            'status' => ['required'],
+            'image_path' => ['nullable', 'image', 'mimes:png,jpg,jpeg', 'max:1999'],
+        ]);
+
+        try {
+
+            $category = new Category();
+
+            if ($request->hasFile('image_path'))
+            {
+                ImageServiceSave::deleteOldPublicImage($category->image_path);
+                $imageSave = new ImageServiceSave();
+                $image_path =  $imageSave->customSavePublicPath($request->image_path,'category');
+                $category->image_path = $image_path;
+
+            }
+
+            if ($request->has('parent'))
+            {
+                $category->title = $request->title;
+                $category->status = $request->status;
+                $category->slug = $request->slug;
+                $category->parent_id = $request->parent;
+            } else {
+                $category->title = $request->title;
+                $category->slug = $request->slug;
+                $category->status = $request->status;
+            }
+            $category->save();
+            session()->flash('success', __('messages.New_record_saved_successfully'));
+            return redirect()->route('admin.category.index');
+        } catch (\Exception $ex) {
+            return view('errors_custom.model_store_error');
+        }
 
     }
 }
---
+++ app/Services/image/ImageService.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace App\Services\image;
+
+
+class ImageService extends ImageServiceTools
+{
+
+    // for save image in to public directory
+    public function save($image)
+    {
+        $this->setImage($image);
+        $this->provider();
+        $result = $image->move(public_path($this->getFinalImgDirectory()), $this->getFinalImgName());
+        return $result ? $this->getImageAddress() : false;
+    }
+
+    // for save image in to storage directory
+    public function moveToStorage($image)
+    {
+        $this->setImage($image);
+        $this->provider();
+        // $result = $file->move(storage_path( self::STORAGE . $this->getFinalFileDirectory()), $this->getFinalFileName());
+        $result = $image->storeAs($this->getFinalImgDirectory(),$this->getFinalImgName(),'public');
+        return $result ? $this->getImageAddress() : false;
+    }
+
+    // for delete image
+    public function deleteImage($filePath)
+    {
+        if (file_exists($filePath)) {
+            unlink($filePath);
+        }
+    }
+
+    // for delete directory & file
+    public function deleteDirectoryAndImages($directory)
+    {
+
+        if (!is_dir($directory)) {
+            return false;
+        }
+
+        $files = glob($directory . DIRECTORY_SEPARATOR . '*',GLOB_MARK);
+        foreach ($files as $file)
+        {
+            if (is_dir($file))
+            {
+                $this->deleteDirectoryAndImages($file);
+            } else {
+                unlink($file);
+            }
+        }
+
+        return rmdir($directory);
+    }
+}
