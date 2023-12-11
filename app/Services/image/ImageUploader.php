036ab622143a2d62f50f84bb48e230589165119b
mason.hows11 <mason.hows11@gmail.com>
2023-12-11 06:32:14 +0330

    add class for upload image

---
+++ app/Services/image/ImageUploader.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Services\image;
+
+
+class ImageUploader
+{
+        public static function upload($image,$path,$model)
+        {
+
+        }
+
+        public static function uploadMany(array  $images,$path,$disk = 'public_storage')
+        {
+
+        }
+}
