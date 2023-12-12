66b31e250b1ecbaee70c660db92304de2869feca
mason.hows11 <mason.hows11@gmail.com>
2023-12-12 10:01:14 +0330

    edit product updated

--- app/Http/Controllers/Admin/Product/AdminProductController.php
+++ app/Http/Controllers/Admin/Product/AdminProductController.php
@@ -88,6 +88,45 @@ class AdminProductController extends Controller
         try {
 
             dd($request);
+            $user = User::first();
+            $updated = null;
+
+            $product = Product::create([
+                'title' => $request->title,
+                'description' => $request->description,
+                'seo_desc' => $request->seo_desc,
+                'product_tags' => $request->product_tags,
+                'status' => $request->status,
+                'price' => $request->price,
+                'user_id' => $user->id,
+            ]);
+            $product->categories()->sync($request->categories);
+            // for upload file
+            $basPath = 'products/' . $product->id . '/';
+            $sourceImagePath = $basPath . 'source_url' . $request->source_url->getClientOriginalName();
+
+            $images = [
+                'thumbnail_path' => $request->thumbnail_path,
+                'demo_url' => $request->demo_url,
+            ];
+
+            $images_path = ImageUploader::uploadMany($images, $basPath);
+            ImageUploader::upload($request->source_url, $sourceImagePath, 'local_storage');
+
+            $updated = $product->update([
+                'thumbnail_path' => $images_path['thumbnail_path'],
+                'demo_url' => $images_path['demo_url'],
+                'source_url' => $sourceImagePath,
+            ]);
+
+            if (!$updated) {
+                session()->flash('warning', __('messages.An_error_occurred_while_uploading_images'));
+                return redirect()->back();
+            }
+
+
+            session()->flash('success', __('messages.New_record_saved_successfully'));
+            return redirect()->route('admin.product.index');
 
         } catch (\Exception $ex) {
 
