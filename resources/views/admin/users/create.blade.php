a163edb33dd168e1a7bd178d0bdc0a72662a5fff
mason.hows11 <mason.hows11@gmail.com>
2023-12-12 19:52:24 +0330

    users create view blade updated

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -9,7 +9,7 @@
 
     <div class="container-fluid">
 
-        <div class="col-xl-6">
+        <div class="col">
 
             <div class="card card-flush h-lg-100" id="kt_contacts_main">
                 <div class="card-header pt-7" id="kt_chat_contacts_header">
@@ -28,13 +28,15 @@
 
                 <div class="card-body pt-5">
 
-                    <form id="kt_ecommerce_settings_general_form" class="form fv-plugins-bootstrap5 fv-plugins-framework" action="#">
-
+                    <form action="{{ route('admin.user.store') }}" method="post" id="kt_ecommerce_settings_general_form" class="form fv-plugins-bootstrap5 fv-plugins-framework" >
+                        @csrf
                         <div class="mb-7">
 
                             <label class="fs-6 fw-semibold mb-3">
                                 <span>بروزرسانی آواتار</span>
-                                <span class="ms-1" data-bs-toggle="tooltip" aria-label="همه بدهکار هستیم file types: png, jpg, jpeg." data-bs-original-title="همه بدهکار هستیم file types: png, jpg, jpeg." data-kt-initialized="1">
+                                <span class="ms-1" data-bs-toggle="tooltip"
+                                      aria-label="همه بدهکار هستیم file types: png, jpg, jpeg."
+                                      data-bs-original-title="همه بدهکار هستیم file types: png, jpg, jpeg." data-kt-initialized="1">
 																	<i class="ki-duotone ki-information fs-7">
 																		<span class="path1"></span>
 																		<span class="path2"></span>
@@ -45,7 +47,10 @@
 
                             <div class="mt-1">
 
-                                <style>.image-input-placeholder { background-image: url('assets/media/svg/files/blank-image.svg'); } [data-bs-theme="dark"] .image-input-placeholder { background-image: url('assets/media/svg/files/blank-image-dark.svg'); }</style>
+                                <style>.image-input-placeholder
+                                    { background-image: url({{ asset('admin_assets/images/svg/blank-image.svg') }}); }
+                                    [data-bs-theme="dark"] .image-input-placeholder
+                                    { background-image: url({{ asset('admin_assets/images/svg/blank-image-dark.svg') }}); }</style>
 
                                 <div class="image-input image-input-outline image-input-placeholder image-input-empty image-input-empty" data-kt-image-input="true">
                                     <div class="image-input-wrapper w-100px h-100px" style="background-image: url('')"></div>
@@ -55,7 +60,7 @@
                                             <span class="path1"></span>
                                             <span class="path2"></span>
                                         </i>
-                                        <input type="file" name="avatar" accept=".png, .jpg, .jpeg">
+                                        <input type="file" name="image_path" accept=".png, .jpg, .jpeg">
                                         <input type="hidden" name="avatar_remove">
                                     </label>
                                     <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" aria-label="انصراف avatar" data-bs-original-title="انصراف avatar" data-kt-initialized="1">
