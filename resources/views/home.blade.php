a4fd4bb9f29f58b8eac7f5370cb74d8321aecefb
mason.hows11 <mason.hows11@gmail.com>
2023-12-13 08:00:24 +0330

    ready for front section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,56 +12,40 @@
 <!-- start header -->
 @include('front.layouts.include.header')
 <!-- end header -->
-
 <!-- start responsive header-->
 @include('front.layouts.include.header_responsive')
 <!-- end responsive header-->
-
 <!-- start main menu -->
 @include('front.layouts.include.nav')
 <!-- end main menu -->
-
 <!-- start main slider -->
 @include('front.layouts.include.main_slider')
 <!-- end main slider -->
-
 <!-- start main content -->
 <main>
     <div class="container">
-
         <!-- start ads -->
         @include('front.layouts.include.main_top_adds')
         <!-- end ads -->
-
-
         <!-- start product slider -->
         @include('front.layouts.include.product_slider')
         <!-- end product slider -->
-
         <!-- start ads -->
         @include('front.layouts.include.main_bottom_ads')
         <!-- end ads -->
-
         <!-- start category box -->
         @include('front.layouts.include.categories')
         <!-- end category box -->
-
-
         <!-- start blog box -->
         @include('front.layouts.include.blog')
         <!-- end blog box -->
-
     </div>
 </main><!-- end main -->
-
 <!-- start footer -->
 @include('front.layouts.include.footer')
 <!-- end footer -->
-
 <!-- scripts -->
 @include('front.layouts.footer_scripts')
 <!--end scripts -->
 </body>
-
-
 </html>
