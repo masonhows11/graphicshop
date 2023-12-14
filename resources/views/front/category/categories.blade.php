879146ba8f7aea4ec4a205f28867f6da7bbc5a64
mason.hows11 <mason.hows11@gmail.com>
2023-12-15 00:06:18 +0330

    style nested category dropdown updated 80%

--- resources/views/front/category/categories.blade.php
+++ resources/views/front/category/categories.blade.php
@@ -1,12 +1,16 @@
 <div class="ps-0 d-flex flex-column accordion-item">
     @foreach( $categories as $child )
         <div class="d-flex mt-2 flex-row">
-            @if( $child->children->count() > 0)
+            <i data-bs-toggle="collapse"
+               data-bs-target="#collapseOne"
+               aria-expanded="true"
+               aria-controls="collapseOne" class="font-11 mt-1 ms-2 fa fa-xs fa-chevron-left"></i>
+           {{-- @if( $child->children->count() > 0)
                 <i data-bs-toggle="collapse"
                    data-bs-target="#collapseOne"
                    aria-expanded="true"
                    aria-controls="collapseOne" class="font-11 mt-1 ms-2 fa fa-xs fa-chevron-left"></i>
-            @endif
+            @endif--}}
             <span class="ms-2">
             <a href="{{ route('search.category',['slug' => $child->title]) }}" class="font-12 d-inline">{{ $child->title }}</a>
         </span>
