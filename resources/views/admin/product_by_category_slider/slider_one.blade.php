1fce93c65b06e04b125f218949c106848ef239b0
mason.hows11 <mason.hows11@gmail.com>
2023-12-20 20:38:23 +0330

    add sliders models & migrations

--- resources/views/admin/product_by_category_slider/slider_one.blade.php
+++ resources/views/admin/product_by_category_slider/slider_one.blade.php
@@ -1,1 +1,93 @@
-<?php
+@extends('dash.include.master_dash')
+@section('dash_page_title')
+
+@endsection
+@section('breadcrumb')
+@endsection
+@section('dash_main_content')
+
+    <div class="container-fluid">
+
+        <div class="row d-flex justify-content-start my-4 bg-white">
+            <div class="col-lg-4 col-md-4 col  my-5  border-bottom title-add-to-stock">
+                <div class="alert my-4">
+                    <h3></h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="row  order-list bg-white">
+            <div class=" my-5">
+                <table class="table table-striped">
+                    <thead class="border-bottom-3 border-top-3">
+                    <tr class="text-center">
+
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center list-stock-paginate">
+            <div class="col-lg-2 col-md-2 ">
+                {{ $orders->links() }}
+            </div>
+        </div>
+    </div>
+
+@endsection
+@push('dash_custom_script')
+    <script type="text/javascript">
+        window.addEventListener('show-delete-confirmation', event => {
+            Swal.fire({
+                title: 'آیا مطمئن هستید این ایتم حذف شود؟',
+                icon: 'error',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'بله حذف کن!',
+                cancelButtonText: 'خیر',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Livewire.emit('deleteConfirmed')
+                }
+            });
+        })
+    </script>
+    <script>
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top',
+            showConfirmButton: false,
+            showCloseButton: true,
+            timer: 5000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        });
+        window.addEventListener('show-result', ({detail: {type, message}}) => {
+            Toast.fire({
+                icon: type,
+                title: message
+            })
+        })
+        @if( session()->has('warning') )
+        Toast.fire({
+            icon: 'warning',
+            title: '{{ session()->get('warning') }}'
+        })
+        @elseif( session()->has('success'))
+        Toast.fire({
+            icon: 'success',
+            title: '{{ session()->get('success') }}'
+        })
+        @endif
+    </script>
+@endpush
+
+
+
