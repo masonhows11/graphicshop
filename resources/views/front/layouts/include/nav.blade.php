adb8c7124cbe5f4849e271b557bafced8a4ab57f
mason.hows11 <mason.hows11@gmail.com>
2023-12-21 11:30:08 +0330

    front basket link updated

--- resources/views/front/layouts/include/nav.blade.php
+++ resources/views/front/layouts/include/nav.blade.php
@@ -9,7 +9,8 @@
                         <li class="col-3 mega-menu-box">
                             <ul>
                                 <li class="menu-title">
-                                    <a href="{{ route('search.category',['slug' => $child->title]) }}"><i class="fa fa-angle-left me-2"></i>{{ $child->title}}</a>
+                                    <a href="{{ route('search.category',['slug' => $child->title]) }}"><i
+                                            class="fa fa-angle-left me-2"></i>{{ $child->title}}</a>
                                 </li>
                                 @if( $child->children != null  )
                                     @include('front.layouts.partials.child_category',['category' => $child->children])
@@ -22,10 +23,13 @@
             </li>
             <li><a href="{{ route('not.found') }}">تخفیف‌ها و پیشنهادها</a></li>
             @guest
-            <li><a href="{{ route('auth.login.form') }}">ثبت نام / ورود</a></li>
+                <li><a href="{{ route('auth.login.form') }}">ثبت نام / ورود</a></li>
+                <li><a href="#">سبد خرید</a></li>
             @endguest
+            @auth
+                <li><a href="{{ route('cart.check') }}">سبد خرید</a></li>
+            @endauth
             <li><a href="{{ route('not.found') }}">وبلاگ</a></li>
-            <li><a href="#">سبد خرید</a></li>
             <li><a href="{{ route('contact_us') }}">تماس با ما</a></li>
             <li><a href="{{ route('about_us') }}">درباره ما</a></li>
         </ul>
