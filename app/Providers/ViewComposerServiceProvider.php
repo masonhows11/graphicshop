0e733ebdd99d555b10cf2154b9354297c5f39972
mason.hows11 <mason.hows11@gmail.com>
2023-12-21 10:46:26 +0330

    front home product by category slider updated 95%

--- app/Providers/ViewComposerServiceProvider.php
+++ app/Providers/ViewComposerServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\SliderOne;
+use App\Models\SliderThree;
+use App\Models\SliderTwo;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -40,11 +42,27 @@ class ViewComposerServiceProvider extends ServiceProvider
         });
         // for visit card category
         View::composer(['front.layouts.product_slider.slider_visit_card'],function ($view){
-            $view->with('products',Category::find(2)->products()->take(4)->get() ?? null);
+            $slider = SliderTwo::first();
+            $products = null;
+            $category_title = null;
+            if($slider)
+            {
+                $products = Product::where('category_id','=',SliderTwo::select('category_id')->first()->category_id)->get();
+                $category_title = $slider->category_name;
+            }
+            $view->with(['products' => $products != null ? $products : null,'category_title' => $category_title != null ? $category_title : null ]);
         });
         // for tract category
         View::composer(['front.layouts.product_slider.slider_tract'],function ($view){
-            $view->with('products',Category::find(3)->products()->take(4)->get() ?? null);
+            $slider = SliderThree::first();
+            $products = null;
+            $category_title = null;
+            if($slider)
+            {
+                $products = Product::where('category_id','=',SliderThree::select('category_id')->first()->category_id)->get();
+                $category_title = $slider->category_name;
+            }
+            $view->with(['products' => $products != null ? $products : null,'category_title' => $category_title != null ? $category_title : null ]);
         });
 
     }
