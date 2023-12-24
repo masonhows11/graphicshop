5682ff1225dbece968e4096550b426e166e2e95a
mason.hows11 <mason.hows11@gmail.com>
2023-12-24 08:17:30 +0330

    home product slider add condition for show product image

--- resources/views/front/layouts/product_slider/slider_visit_card.blade.php
+++ resources/views/front/layouts/product_slider/slider_visit_card.blade.php
@@ -11,7 +11,13 @@
                     @foreach( $products as $product)
                         <div class="item">
                             <div class="card border-0 custom-card mt-3">
-                                <a href="{{ route('product',$product->title) }}" class="d-block w-100"><img src="{{ asset($product->thumbnail_path) }}" class="slider-pic"></a>
+                                <a href="{{ route('product',$product->title) }}" class="d-block w-100">
+                                    @if($product->thumbnail_path != null )
+                                        <img src="{{ asset('storage/'.$product->thumbnail_path) }}" alt="{{ $product->title . $product->thumbnail_path }}" class="slider-pic"></a>
+                                @else
+                                    <img src="{{ asset('default_image/no-image-icon-23494.png') }}" alt="no-image"
+                                         class="slider-pic">
+                                @endif
                                 <div class="card-body">
                                     <a href="{{ route('product',$product->title) }}" class="product-title">{{ $product->title }}</a>
                                     <div class="d-flex justify-content-between">
