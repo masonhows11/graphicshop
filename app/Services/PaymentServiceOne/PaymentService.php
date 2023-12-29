f1ff9916747caf6eb5525f09ed51d7dfee9ef3a2
mason.hows11 <mason.hows11@gmail.com>
2023-12-29 21:53:21 +0330

    add sandbox payment config file

--- app/Services/PaymentServiceOne/PaymentService.php
+++ app/Services/PaymentServiceOne/PaymentService.php
@@ -4,11 +4,14 @@
 namespace App\Services\PaymentServiceOne;
 
 
+use Illuminate\Support\Facades\Config;
+
 class PaymentService
 {
     public function zarinpal()
     {
         $amount = 0;
+        $merchantId = Config::get('Payment.zarinpal_sandbox_merchantId');
     }
 
 }
