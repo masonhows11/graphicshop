2c61c7873ceb4c25c5d48ef17b1201e3d631a5af
mason.hows11 <mason.hows11@gmail.com>
2024-01-03 22:17:41 +0330

    view provider clear updated

--- app/Providers/ViewComposerServiceProvider.php
+++ app/Providers/ViewComposerServiceProvider.php
@@ -3,10 +3,6 @@
 namespace App\Providers;
 
 use App\Models\Category;
-use App\Models\Product;
-use App\Models\SliderOne;
-use App\Models\SliderThree;
-use App\Models\SliderTwo;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -28,44 +24,5 @@ class ViewComposerServiceProvider extends ServiceProvider
             $view->with(['categories' => Category::tree()->get()->toTree(),
                         'mainCategories'=> Category::where(['parent_id'=>null,'status' => 1])->take(10)->get() ]);
         });
-
-        // for banner category
-      /*  View::composer(['front.layouts.product_slider.slider_banner'],function ($view){
-            $slider = SliderOne::first();
-            $products = null;
-            $category_title = null;
-            if($slider)
-            {
-                $products = Product::where('category_id','=',SliderOne::select('category_id')->first()->category_id)->get();
-                $category_title = $slider->category_name;
-            }
-            $view->with(['products' => $products != null ? $products : null,'category_title' => $category_title != null ? $category_title : null ]);
-        });*/
-        // for visit card category
-      /*  View::composer(['front.layouts.product_slider.slider_visit_card'],function ($view){
-            $slider = SliderTwo::first();
-            $products = null;
-            $category_title = null;
-            if($slider)
-            {
-                $products = Product::where('category_id','=',SliderTwo::select('category_id')->first()->category_id)->get();
-                $category_title = $slider->category_name;
-            }
-            $view->with(['products' => $products != null ? $products : null,'category_title' => $category_title != null ? $category_title : null ]);
-
-        });*/
-        // for tract category
-       /* View::composer(['front.layouts.product_slider.slider_tract'],function ($view){
-            $slider = SliderThree::first();
-            $products = null;
-            $category_title = null;
-            if($slider)
-            {
-                $products = Product::where('category_id','=',SliderThree::select('category_id')->first()->category_id)->get();
-                $category_title = $slider->category_name;
-            }
-            $view->with(['products' => $products != null ? $products : null,'category_title' => $category_title != null ? $category_title : null ]);
-        });*/
-
     }
 }
