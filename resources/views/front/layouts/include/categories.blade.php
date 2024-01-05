2fd3e7adc8f2f3aee7ece1796546277e03bab20d
mason.hows11 <mason.hows11@gmail.com>
2024-01-05 19:20:00 +0330

    add link to category section in main content home page

--- resources/views/front/layouts/include/categories.blade.php
+++ resources/views/front/layouts/include/categories.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
         @forelse( $mainCategories as $category)
             <div class="col-4 col-md-3 col-lg text-center">
-                <a href="javascript:void(0)">
+                <a href="{{ route('search.category',$category->title) }}">
                     <img src="{{ $category->image_path ? asset('storage/images/category/'.$category->image_path) : asset('default_image/no-image-icon-23494.png') }}" height="64" width="64"  class="mt-2 mb-2" alt="category-image">
                     <p>{{ $category->title }}</p></a>
             </div>
